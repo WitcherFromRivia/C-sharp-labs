c8f26af735da9058f86e5bfeba9617063db70b03
WitcherFromRivia <48452428+WitcherFromRivia@users.noreply.github.com>
2019-03-13 18:30:51 +0200

    Add files via upload

---
+++ Lab1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Введите число a :");
+            int a = Convert.ToInt32( Console.ReadLine());
+            Console.WriteLine("Ответ:"+ (a*a*a));
+            Console.ReadLine();
+
+        }
+    }
+}
